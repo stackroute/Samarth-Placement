c8a85871528342b70a17cfd1ff0acf0b6a4a15dd
Ankit Vashist <ankitvashist11@gmail.com>
2016-11-27 20:19:14 +0000

    update of jobsearch

---
+++ webapp/jobSearch/component/jobSearch.js
@@ -0,0 +1,21 @@
+
+angular
+
+     .module('instantsearch')
+
+/*     angular.module('instantsearch',['ngMaterial','ui.router','ngMdIcons'])
+*/
+/* creating component for job search*/
+   .component("jobSearch",{
+   	templateUrl:'../template/jobSearch.html',
+   	// bindings:{
+   	// 	name :"=",
+   	// 	txt:'@'
+   	// },
+   	// controller:function()
+   	// {
+   	// 	this.searchString="Value changed";
+   	// 	this.ankit="my name is ankit";
+   	// }
+   })
+  
