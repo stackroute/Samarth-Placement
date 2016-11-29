23e8c40324e9aea0c7d56316eb17365b4d16ecc2
rishu <riteshgarg9999@gmail.com>
2016-11-29 15:37:44 +0530

    repaired for the working

--- webapp/app.js
+++ webapp/app.js
@@ -1,21 +1,21 @@
 
-angular.module('samarth',
- ['ngMaterial',
- 'ngMdIcons',
- 'ui.router',
- 'ngMessages',
- 'samarth.home',
- 'samarth.dashboard',
- 'samarth.jobPost',
- 'samarth.jobSearch',
- 'samarth.cordsignup'
- 'samarth.candidateReg',
- 'samarth.jobSearch'
-  ])
+angular
+	.module('samarth',
+		 ['ngMaterial',
+		 'ngMdIcons',
+		 'ui.router',
+		 'ngMessages',
+		 'samarth.home',
+		 'samarth.dashboard',
+		 'samarth.jobPost',
+		 'samarth.jobSearch',
+		 'samarth.cordsignup',
+		 'samarth.candidateReg',
+		 'samarth.coordinatorlogin'
+		  ])
 
-    .config(function($mdThemingProvider) {
-
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      
-  });
+	.config(function($mdThemingProvider) {
+	    $mdThemingProvider.theme('default')
+	      .primaryPalette('blue')
+	      
+	  });
--- webapp/coordinatorLogin/controller/signinCtrl.js
+++ webapp/coordinatorLogin/controller/signinCtrl.js
@@ -1,25 +1,23 @@
-
-(function(){
+(function() {
+  'use strict';
+  angular
+  .module('samarth.coordinatorlogin')
+  .controller('signinCtrl',signinCtrl);
   
+  signinCtrl.$inject=['myService','$state'];
 
-
-angular
-.module('samarth.coordinatorlogin')
-.controller('signinCtrl',signinCtrl);
- signinCtrl.$inject=['myService','$state'];
-function signinCtrl(myService,$state)
- 
+  function signinCtrl(myService,$state)
   {
     var vm =this;
-     vm.user={
-           };
+    vm.user={
+    };
     vm.signIn=signIn;
-       vm.errormsg="";
+    vm.errormsg="";
     function signIn()
     { 
-      
-       myService.auth().then(function(response) {
-     
+
+     myService.auth().then(function(response) {
+
        var dataCounter =0;
        var testCounter=0;
        
@@ -33,25 +31,25 @@ function signinCtrl(myService,$state)
            console.log("true");
            
            testCounter++;
-            $state.go('index.signin.dashboard')
+           $state.go('index.signin.dashboard')
          }
-        else if((email!=vm.user.emailAddress || password!=vm.user.password ) &&  response.data.length== dataCounter  && testCounter==0)
-        {
-         vm.errormsg=" Login failed ! email or password is not correct"
+         else if((email!=vm.user.emailAddress || password!=vm.user.password ) &&  response.data.length== dataCounter  && testCounter==0)
+         {
+           vm.errormsg=" Login failed ! email or password is not correct"
 
-         $state.go('index') 
-       }
+           $state.go('index') 
+         }
+       });
      });
-     });
-     }
-}
+   }
+ }
 })();
   //   $scope.signIn = function($scope,myService,$state) {
   //     myService.auth().then(function(response) {
 
   //      var dataCounter =0;
   //      var testCounter=0;
-       
+
   //      response.data.map(function(data)
   //      { 
   //        var email=data.email;
@@ -60,7 +58,7 @@ function signinCtrl(myService,$state)
   //        if(email==$scope.user.emailAddress && password==$scope.user.password)
   //        {
   //          console.log("true");
-           
+
   //          testCounter++;
   //           $state.go('index.dashboardpage')
   //        }
