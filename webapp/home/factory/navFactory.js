a28c95a1342ef97febc937655757e096a3c0dd29
kumaridevi <mkumaridevi11@gmail.com>
2016-11-29 07:29:20 +0000

    removed merge conflict in home factory

--- webapp/home/factory/navFactory.js
+++ webapp/home/factory/navFactory.js
@@ -1,15 +1,8 @@
 (function(){
-<<<<<<< HEAD
   'use strict'
   angular
     .module('samarth.home')
-=======
-	'use:strict'
-	angular
-	.module('samarth.home')
->>>>>>> d0bfe8fe804ef3d6633ce7ba3bfa418f162fe334
 	.factory('navFactory', navFactory);
-
 	function navFactory($http) {
 		var factory = {getSidenav:getSidenav};
 		return factory;
