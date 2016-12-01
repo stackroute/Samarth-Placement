23e36ff01b6bb86932fcfe015cd48b1635b753fe
kumaridevi <mkumaridevi11@gmail.com>
2016-12-01 10:09:19 +0000

    modified factory

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express=require('express');
 var app=express();
 var path=require('path');
 var profobject=require('./webapp/coordinatorReg/json/prof.js');
+var navItems = require('./webserver/navigationLinks/navigateRouter.js');
 var bodyParser = require('body-parser');
 // var Bear     = require('./bear.js');
 // configure app to use bodyParser()
@@ -25,6 +26,8 @@ app.get('/createaccount',function(req,res){
 app.get('/createaccount/submit',function(req,res){
     res.send();
 });
+
+app.get('/sidenavbar', navItems);
 
 // var router = express.Router();
 
