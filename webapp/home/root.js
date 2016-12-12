d819c8d97c5603bbe3938f888bb84303dce1f619
shinderpalsingh <shinders1993@gmail.com>
2016-12-12 14:04:27 +0000

    modified authbytoken file in webserver

---
+++ webapp/home/root.js
@@ -0,0 +1,18 @@
+angular.module('samarth')
+   .config(config);
+
+   function config($stateProvider){
+     
+     $stateProvider
+    .state('initialState', {
+           url: '',
+           views: {
+               'content': {
+                   controller: 'initialCtrl'
+
+               }
+           }
+       });
+
+}
+
--- webserver/auth/users.js
+++ webserver/auth/users.js
@@ -1,56 +1,4 @@
-// let mongoose = require('mongoose');
-// let bCrypt = require('bcrypt-nodejs');
 
-// let schema = mongoose.Schema({
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         trim: true,
-//         min: 10
-//     },
-//     hash_pwd: {
-//         type: String,
-//         required: true
-//     },
-//     status: {
-//         type: String,
-//         default: 'active',
-//         enum: ['active', 'disabled']
-//     },
-//     createdon: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     lastseenon: {
-//         type: Date,
-//         default: Date.now
-//     }
-// });
-
-// // //virtuals
-// schema
-//     .virtual('pwd')
-//     .set(function(pwd) {
-//         this._pwd = pwd;
-//         this.hash_pwd = this.encryptPassword(pwd);
-//     })
-//     .get(function() {
-//         return this._pwd;
-//     });
-
-// schema.methods = {
-//     validPassword: function(plainText) {
-//         return bCrypt.compareSync(plainText, this.hash_pwd);
-//     },
-
-//     encryptPassword: function(plainText) {
-//         return bCrypt.hashSync(plainText, bCrypt.genSaltSync(10), null);
-//     }
-// };
-
-
-// module.exports = mongoose.model('users', schema, 'users');
 
 
 var mongoose = require('mongoose');
@@ -92,5 +40,4 @@ login.methods.validPassword = function(password) {
 
 module.exports = {
     "login": login
-    // "sidenavcontent": sidenavcontent
-};
+  };
