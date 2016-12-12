fcf5695b5594ce66d59cc7778be3e6a1ddc7e9cd
shinderpalsingh <shinders1993@gmail.com>
2016-12-12 10:46:36 +0000

    modified authentication files of webserver

---
+++ webserver/auth/authbytoken.js
@@ -0,0 +1,85 @@
+let jwt = require('jsonwebtoken');
+let UserModel = require('./users');
+let authCoordinator = require('./authCoordinator');
+var mongoose = require('mongoose');
+var coordinatoruser = mongoose.model('coordinatorusers', UserModel.login);
+
+var signin = function(email, pwd, callback, unauthCB) {
+
+    coordinatoruser.findOne({
+            email: email
+        },
+
+
+        function(err, user) {
+            if (err) {
+
+                console.error("Database error in finding user, error: ", err);
+                callback({
+                    error: "Failed to process request, please try later..!"
+                }, null)
+                return;
+            }
+
+            if (!user) {
+
+                console.error('User ', email, ' not found..!');
+                unauthCB({
+                    error: "Invalid credentials...!"
+                }, null);
+                return;
+            }
+
+            if (!user.validPassword(pwd)) {
+                unauthCB({
+                    error: "Invalid credentials...!"
+                });
+                return;
+            }
+
+            
+            authCoordinator.getCandidateAuthToken(user).then(
+                function(details) {
+                    
+               
+                    var sessionUser = {
+                        "email": user.email,
+                        "cid": details.coordinator.coordinatorId,
+                        "name": details.coordinator.coordinatorName,
+                        "gender": details.coordinator.coordinatorGender,
+                        "location": details.coordinator.coordinatorLocation,
+                        "role": details.coordinator.coordinatorRole,
+                        "sm-token": "TBD"
+                    };
+
+                    console.log("Got token successfully ", sessionUser);
+
+                    generateJWTToken(sessionUser, callback); //generate JWTToken
+                },
+                function(err) {
+                    callback(err);
+                }
+            );           
+        }); 
+}; 
+
+var generateJWTToken = function(user, cb) {
+    var payload = user;
+    var secretOrPrivateKey = 'SAMARTH-WEBAPP-SECRET';
+    var options = {
+        algorithm: "HS256",
+        expiresIn: 36000,
+        issuer: user.email
+    };
+
+    jwt.sign(payload, secretOrPrivateKey, options, function(err, jwtToken) {
+        console.log("Sending token ", user, jwtToken);
+        cb(err, user, jwtToken);
+    });
+}
+
+module.exports = {
+    "signin": signin
+};
+
+
---
+++ webserver/auth/authrouter.js
@@ -0,0 +1,64 @@
+let router = require('express').Router();
+let authByToken = require('./authbytoken');
+var UserModel = require("./users");
+let bodyParser = require('body-parser');
+var apiRoutes = require('express').Router();
+var jsonBodyParser = bodyParser.json();
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
+
+
+apiRoutes.post('/signin', function(req, res) {
+
+    if (!req.body.email || !req.body.pwd) {
+
+        res.json({
+            error: "Please try with valid credentials..!"
+        });
+        return;
+    }
+
+    try {
+
+        authByToken.signin(req.body.email, req.body.pwd,
+            function(err, user, jwtToken) {
+                if (err) {
+
+                    return res.status(500).json({
+                        error: "Internal error in processing request, please retry later..!"
+                    });
+                }
+
+                if (!jwtToken) {
+
+                    console.error("Empty token generated...!");
+                    return res.status(403).json({
+                        error: "Internal error in processing request, please retry later..!"
+                    });
+                } else {
+
+                    user['token'] = jwtToken;
+                    return res.status(200).json(user);
+                }
+            },
+            function(err) {
+                return res.status(403).json(err);
+            });
+    } catch (err) {
+        console.error("Error in signin ", err);
+        return res.status(500).json({
+            error: "Internal error in processing request, please retry later..!"
+        });
+    }
+
+}); 
+
+module.exports = apiRoutes;
+
+
+
+
+
+
