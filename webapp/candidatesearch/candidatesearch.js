1fce1a33a05a3029a0293047cc5c727c09e197fb
rishu <riteshgarg9999@gmail.com>
2016-12-14 20:03:27 +0530

    repaired candidate search

---
+++ webapp/candidatesearch/candidatesearch.js
@@ -0,0 +1,33 @@
+angular.module('samarth.candidatesearch',[])
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+        .state('index.candidatesearch', {
+            url: 'candidatessearch/:circleDomain?circleName',
+            views: {
+                "content@": {
+                    templateUrl: 'candidatesearch/templates/candidatesearchhome.html',
+                    controller: 'candidatesearchctrl'
+                        }
+                    },
+                resolve:{
+                    circleDomain:['$stateParams',function($stateParams){
+                        return $stateParams.circleDomain;
+                    }],
+                    circleName:['$stateParams',function($stateParams){
+                        return $stateParams.circleName;
+                    }]
+                }
+            })
+        .state('index.candidatesearch.results', {
+            url: 'searchlist',
+            views: {
+                "results": {
+                    templateUrl: 'candidatesearch/templates/candidatesearchresults.html',
+                            //controller: 'candidatesearchctrl'
+                        }
+                    }
+                })
+    }
+    ]);
