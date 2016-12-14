242986c513db9f2d892b9e0a0867b96eb1724199
Ankit Vashist <ankitvashist11@gmail.com>
2016-12-14 10:42:40 +0000

    updating code for testing

--- test/jobSearchTest.js
+++ test/jobSearchTest.js
@@ -1,25 +1,32 @@
 var assert = require('chai').assert;
 var request = require('supertest');
-
 var host_url = 'http://localhost:8081'
-
 container=request(host_url+'/jobProfile/');
-
 describe('JobSearch test cases',function(){
 describe('checking API calling',function(){
 	describe(' checking job profile', function(){
-		
 		it("we should get the response while calling",function(done) {
-			//console.log(container_url);
-      container
+	    container
         .get('/getJobs')
         .expect(200, done)
-        //.expect(500,done)
-
+    });
+    it('should be json response', function(done) {
+      container
+        .get('/getJobs')
+        .expect('Content-Type', 'application/json; charset=utf-8', done);
+    });
+    it('we should get the response', function(done) {
+      container
+        .get('/getJobs')
+        .expect(msg, done);
+    });
+    it("we making call to wrong api and checking status for 404 status",function(done) {
+			container
+        .get('/getJob')
+        .expect(404,done)
     });
 	});
 });
-
 describe('Duration ', function(){
 	describe('something slow',function(){
 		this.slow(10000);
