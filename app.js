5fdfc8758471a33601d08410aa41ca2ce29fea1e
thirdperspective <hariprasath.er@gmail.com>
2016-12-14 18:44:58 +0530

    Added the config and using environment values

--- app.js
+++ app.js
@@ -1,52 +1,100 @@
-let proxy = require('http-proxy');
-let express=require('express');
-let path=require('path');
-let morgan = require('morgan');
-
-let mongoose = require('mongoose');
-let cookieParser = require('cookie-parser');
-
-let profobject=require('./webapp/coordinatorReg/json/prof.js');
+//module dependency
+const proxy = require('http-proxy');
+const express=require('express');
+const path=require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const logger = require('./webserver/config/appconfig.js');
+//including environment value using config
+const config = require('./webserver/config/');
 let navItems = require('./webserver/navbar/navigateRouter.js');
 
 let authRoutes = require('./webserver/auth/authrouter');
 let authByToken = require('./webserver/auth/authbytoken');
+  
+function createApp() {
+	const app = express();
 
-mongoose.connect('mongodb://localhost:27017/samarthplatformdb');
-mongoose.set('debug', true);
-
-let app=express();
-
-let port = process.env.PORT || 8080;
-
-let platformProxy = proxy.createProxyServer();
-
-app.onAppStart = function(addr) {
-  console.log("Samarth-Placement web app is now Running on port:", addr.port);
+  return app;
 }
 
-app.use(cookieParser());
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'webapp')))
-app.use(express.static(path.join(__dirname, 'bower_components')))
+function setupStaticRoutes(app) {
+  app.use(express.static(path.join(__dirname, 'webapp')));
+	app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', authRoutes);
-app.use('/', navItems);
+  return app;
+}
 
-app.use('/', function(req, res) {
+function setupMiddlewares(app) {
+  app.use(cookieParser());
+	app.use(morgan('dev'));
+  return app;
+}
 
-  let options = {
-    target: {
-      host: 'localhost',
-      port: 8081
-    }
-  };
-  platformProxy.web(req, res, options);
+function setupMongooseConnections() {
+  mongoose.connect(config.MONGO_URL);
 
-});
+  mongoose.connection.on('connected', function() {
+   /* logger.debug('Mongoose is now connected to ', config.MONGO_URL);*/
+  });
 
-platformProxy.on('error', function(err, req, res) {
-  console.error('Error in proxy pass: ', err);
-});
+  mongoose.connection.on('error', function(err) {
+/*    logger.error('Error in Mongoose connection: ', err);
+*/  });
 
-module.exports = app;
+  mongoose.connection.on('disconnected', function() {
+/*    logger.debug('Mongoose is now disconnected..!');
+*/  });
+
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+      /*logger.info(
+        'Mongoose disconnected on process termination'
+      );*/
+      process.exit(0);
+    });
+  });
+}
+
+
+function setupRestRoutes(app) {
+  app.use('/', authRoutes);
+  app.use('/', navItems);
+  app.use('/', function(req, res) {
+
+    let options = {
+      target: {
+        host: 'localhost',
+        port: 8081
+      }
+    };
+    platformProxy.web(req, res, options);
+  });
+
+  platformProxy.on('error', function(err, req, res) {
+    console.error('Error in proxy pass: ', err);
+  });
+    return app;
+};
+
+let port = process.env.PORT || 8080;
+let platformProxy = proxy.createProxyServer();
+
+module.exports = function() {
+  
+  let app = createApp();
+
+  app = setupStaticRoutes(app);
+
+
+  app = setupMiddlewares(app);
+
+  app = setupRestRoutes(app);
+  app.onAppStart = function(addr) {
+    console.error('Samarth-Candidateprofile web app is now Running on port:', addr.port);
+};
+  setupMongooseConnections();
+
+  return app;
+};
