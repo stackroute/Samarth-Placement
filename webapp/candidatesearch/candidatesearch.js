d69afd1440e4d0dc2725d5918256887daa35e8c8
rishu <riteshgarg9999@gmail.com>
2016-12-14 19:49:55 +0530

    need latest files

--- webapp/candidatesearch/candidatesearch.js
+++ webapp/candidatesearch/candidatesearch.js
@@ -6,27 +6,20 @@ angular.module('samarth.candidatesearch',[])
         .state('index.candidatesearch', {
             url: 'candidatessearch/:circleDomain?circleName',
             views: {
-                // "appbar@": {
-                //     templateUrl: 'home/templates/appbar.html',
-                //     controller: 'appbarctrl'
-                // },
                 "content@": {
                     templateUrl: 'candidatesearch/templates/candidatesearchhome.html',
                     controller: 'candidatesearchctrl'
-                }
-                        // "footer": {
-                        //     templateUrl: 'home/templates/footer.html',
-                        // }
+                        }
                     },
-                    resolve:{
-                        circleDomain:['$stateParams',function($stateParams){
-                            return $stateParams.circleDomain;
-                        }],
-                        circleName:['$stateParams',function($stateParams){
-                            return $stateParams.circleName;
-                        }]
-                    }
-                })
+                resolve:{
+                    circleDomain:['$stateParams',function($stateParams){
+                        return $stateParams.circleDomain;
+                    }],
+                    circleName:['$stateParams',function($stateParams){
+                        return $stateParams.circleName;
+                    }]
+                }
+            })
         .state('index.candidatesearch.results', {
             url: 'searchlist',
             views: {
@@ -36,23 +29,5 @@ angular.module('samarth.candidatesearch',[])
                         }
                     }
                 })
-        // .state('registercandidate', {
-        //     url: 'registercandidate',
-
-        //     views: {
-        //         "appbar": {
-        //             templateUrl: 'home/templates/appbar.html',
-        //             controller: 'Navbarcontroller'
-        //         },
-        //         "content@": {
-        //             templateUrl: 'candidatesearch/templates/candidateregistration.html',
-        //             controller: 'candidateregistrationctrl'
-        //         },
-        //         "footer": {
-        //             templateUrl: 'home/templates/footer.html',
-        //         }
-        //     }
-
-        // });
     }
     ]);
