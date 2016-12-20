b4a3f62cb833d00d5c97c98cade04d72a520a2c8
Ankit Vashist <ankitvashist11@gmail.com>
2016-12-20 15:18:35 +0000

    updating

--- webapp/jobSearch/controller/jobSearchCtrl.js
+++ webapp/jobSearch/controller/jobSearchCtrl.js
@@ -1,52 +1,62 @@
 (function(){
-  'use strict'
+ 'use strict'
 angular.module('samarth.jobSearch')
-    .controller('jobSearchCtrl', ['$scope',
-      '$stateParams',
-      'Pagination',
-      'jobSearchFactory',
-      function($scope, $stateParams, Pagination,jobSearchFactory) {
-        /* var searchQueryStr = $stateParams.searchstr;
-         jobSearchFactory.searchJobDetails(searchQueryStr).then*/
-      //  $scope.pagination = Pagination.getNew(4);
-        console.log("before indexing");
-      //  if ($stateParams.searchText !== undefined ) {
-      //          $scope.searchJob($stateParams.searchText);
-      //      }
-        $scope.searchJob = function(searchText) {
-          //var arr=key.split(/[ ,]+/);
-          console.log("hello from ctrl");
-          jobSearchFactory.searchJobDetails()
-              .then(function successCallbackfun(response) {
-                 $scope.result = response.data;
-                 console.log($scope.result);
-                 $scope.pagination = Pagination.getNew(4);
-                 $scope.pagination.numPages = Math.ceil(response.data.length / $scope.pagination.perPage);
-                 $scope.message = "";
-                 /*for(int i=0;i<=response.data.length;i++)
-                 {
-                  if(result[i].title != searchText)
-                  {
-                    $scope.message = "No Result Found !";
-                  }
-                 }*/
-                 if (response.data.length == 0) {
-                   $scope.message = "No Result Found ! Please Enter Value For Results";
-                 }
-                 else{
-                   $scope.message="Showing " + response.data.length + " Results for Job Search";
-                 }
-                 },
-                /*function errorCallbackfun(response) {
-                  console.log("some error occured");
-                  $scope.message = "Some Error Occured ";
-                },*/
-                function (err)
-                 {
-                  $scope.message = err;
-                  //console.log($scope.message);
-                 }
-              )
-        };
-    }]);
+  .controller('jobSearchCtrl', ['$scope',
+     '$stateParams',
+     'Pagination',
+     'jobSearchFactory',
+     'circlesGetService',
+     function($scope, $stateParams, Pagination,jobSearchFactory,circlesGetService) {
+       console.log("Inside JobSearch Ctrl before indexing");
+      if ($stateParams.searchText !== undefined ) {
+            $scope.searchJob($stateParams.searchText);
+        }
+        else{
+            // $scope.message = "Enter Text To Search For Jobs";
+        }
+      console.log("params value after checking "+ $stateParams.searchText);
+
+     var profs="";
+     circlesGetService.getCircle()
+        .then(function(response) {
+           $scope.profiling = response.data;
+           for(var i=0;i<response.data.length;i++){
+             profs+=response.data[i].name+"-";
+           }
+            console.log("circles arrray "+profs);
+         }, 
+         function(err) {
+            console.log("circles array "+err);
+         });
+
+       $scope.searchJob = function(searchText) {
+         //var arr=key.split(/[ ,]+/);
+         console.log("params inside fun " + searchText);
+            //jobSearchFactory.searchJobDetails()
+            jobSearchFactory.searchJobs(searchText,profs)
+             .then(function successCallbackfun(response) {
+                $scope.result = response.data;
+                console.log($scope.result);
+                $scope.pagination = Pagination.getNew(4);
+                $scope.pagination.numPages = Math.ceil(response.data.length / $scope.pagination.perPage);
+                $scope.message = "";
+                if (response.data.length == 0) {
+                  $scope.message = "No Result Found ! Enter Text to Get Jobs";
+                }
+                else{
+                  $scope.message="Showing " + response.data.length + " Results for Job Search";
+                }
+                },
+               function errorCallbackfun(response) {
+                 console.log("some error occured");
+                 $scope.message = "Some Error Occured ";
+               },
+               function (err)
+                {
+                 $scope.message = err;
+                 //console.log($scope.message);
+                }
+             )
+       };
+   }]);
 })();
--- webapp/jobSearch/factory/jobSearchFactory.js
+++ webapp/jobSearch/factory/jobSearchFactory.js
@@ -1,33 +1,24 @@
 (function(){
-  'use strict'
+ 'use strict'
 angular
   .module('samarth.jobSearch')
   .factory("jobSearchFactory", ['$http',function($http) {
-    var obj = {};
-    obj.searchJobDetails = function(){
-        //return $http.get('./samarth-webcomponents/jobSearch/jobSearchData/jobSearchData.json');
-        return $http.get('/jobProfile/getJobs');
-    }
-    return obj;
+   var obj = {};
+   obj.searchJobDetails = function(){
+       //return $http.get('./samarth-webcomponents/jobSearch/jobSearchData/jobSearchData.json');
+       return $http.get('/jobProfile/getJobs');
+   }
+
+   obj.searchJobs = function(searchTxt,profs){
+       return $http({
+       method : 'GET',
+       url : '/jobProfile/searchJobs/'+searchTxt+'/'+profs,
+       // params:{
+       //     searchTxt:searchTxt,
+       //     profs:profs
+       // }
+       })
+   }
+   return obj;
 }]);
 })();
-// (function(){
-//   'use strict'
-// angular
-//   .module('samarth.jobSearch')
-//   .factory("jobSearchFactory", ['$http',function($http) {
-//     var obj = {
-//     	searchJobDetails:searchJobDetails,
-//     	searchJobsbyId:searchJobsbyId
-//     };
-//     return obj;
-
-//     obj.searchJobDetails = function(){
-//         //return $http.get('./samarth-webcomponents/jobSearch/jobSearchData/jobSearchData.json');
-//         return $http.get('/jobProfile/getJobs');
-//     }
-//     obj.searchJobsbyId =function(jobcode){
-//     	return $http.get('/jobProfile/'+jobcode);
-//     }
-//   }]);
-// })();
