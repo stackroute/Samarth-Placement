09030ff8720a799b3acdf45e5acbd6befd669371
Ankit Vashist <ankitvashist11@gmail.com>
2016-12-22 07:51:53 +0000

    updating factory code

--- webapp/jobSearch/factory/jobSearchFactory.js
+++ webapp/jobSearch/factory/jobSearchFactory.js
@@ -8,15 +8,10 @@ angular
        //return $http.get('./samarth-webcomponents/jobSearch/jobSearchData/jobSearchData.json');
        return $http.get('/jobProfile/getJobs');
    }
-
    obj.searchJobs = function(searchTxt,profs){
        return $http({
        method : 'GET',
        url : '/jobProfile/searchJobs/'+searchTxt+'/'+profs,
-       // params:{
-       //     searchTxt:searchTxt,
-       //     profs:profs
-       // }
        })
    }
    return obj;
