f6fda9db341ef192d93f0667f00e6e88313a7585
shinderpalsingh <shinders1993@gmail.com>
2016-12-27 07:01:06 +0000

    modified

--- webapp/candidatesearch/candidatesearch.js
+++ webapp/candidatesearch/candidatesearch.js
@@ -2,51 +2,50 @@ angular
   .module('samarth.candidatesearch',[])
   .config(config);
 
-    function config($stateProvider) {
-      let loginRequired = ['$q','$location', '$auth', function($q, $location, $auth) {
-      let deferred = $q.defer();
-      if ($auth.isAuthenticated()) {
-        deferred.resolve();
+  function config($stateProvider) {
+    let loginRequired = ['$q','$location', '$auth', function($q, $location, $auth) {
+    let deferred = $q.defer();
+    if ($auth.isAuthenticated()) {
+      deferred.resolve();
+    }
+    else {
+      $location.path('/home');
+    }
+    return deferred.promise;
+  }];
+  $stateProvider
+  .state('index.candidatesearch', {
+    url: 'candidatessearch/:circleDomain?circleName',
+    params:{
+      circleName:null,
+      circleDomain:null
+    },
+    views: {
+      "content@": {
+        templateUrl: 'candidatesearch/templates/candidatesearchhome.html',
+        controller: 'candidatesearchctrl',
+        resolve: {
+          loginRequired: loginRequired
+        }
       }
-      else {
-        $location.path('/home');
-      }
-      return deferred.promise;
-    }];
-
-    $stateProvider
-      .state('index.candidatesearch', {
-        url: 'candidatessearch/:circleDomain?circleName',
-        params:{
-          circleName:null,
-          circleDomain:null
-        },
-        views: {
-          "content@": {
-            templateUrl: 'candidatesearch/templates/candidatesearchhome.html',
-            controller: 'candidatesearchctrl',
-            resolve: {
-              loginRequired: loginRequired
-            }
-          }
-        },
-      resolve:{
-        circleDomain:['$stateParams',function($stateParams){
-          return $stateParams.circleDomain;
-        }],
-        circleName:['$stateParams',function($stateParams){
-          return $stateParams.circleName;
-        }]
-      }
-    })
-    .state('index.candidatesearch.results', {
-      url: 'searchlist',
-      params:{
-        jobname:null
-      },
-      views: {
-        'results': {
-          templateUrl: 'candidatesearch/templates/candidatesearchresults.html',
+    },
+    resolve:{
+      circleDomain:['$stateParams',function($stateParams){
+        return $stateParams.circleDomain;
+      }],
+      circleName:['$stateParams',function($stateParams){
+        return $stateParams.circleName;
+      }]
+    }
+  })
+  .state('index.candidatesearch.results', {
+    url: 'searchlist',
+    params:{
+      jobname:null
+    },
+    views: {
+      'results': {
+        templateUrl: 'candidatesearch/templates/candidatesearchresults.html',
             // controller: 'candidatesearchctrl'
             resolve: {
               loginRequired: loginRequired
@@ -54,4 +53,4 @@ angular
           }
         }
       })
-    }
+}
--- webapp/placementProcess/placement.js
+++ webapp/placementProcess/placement.js
@@ -6,10 +6,19 @@
 			'ui.router',
 			'ngMessages'
 			])
-
-		.config(placementProcessConfig)
-
-		function placementProcessConfig($stateProvider){
+    
+     .config(config);
+      function config($stateProvider) {
+      let loginRequired = ['$q','$location', '$auth', function($q, $location, $auth) {
+      let deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      }
+      else {
+        $location.path('/home');
+      }
+      return deferred.promise;
+    }];
 			$stateProvider
 			.state("index.job",{
 				url:"/job/:profession",
@@ -19,20 +28,26 @@
 				views: {
 					'content@': {
 						templateUrl:'./placementProcess/template/job.html',
-						controller:"jobCtrl"
+						controller:"jobCtrl",  
+						resolve: {
+            loginRequired: loginRequired
+            }
+
 					}
 				}
 			})
 			.state("index.candidatePlacement",{
 				url:"/candidatePlacement/:profession?",
 				params:{
-					profession:null,
-					jobcode:null
+					profession:null
 				},
 				views: {
 					'content@': {
 						templateUrl:'./placementProcess/template/candidatePlacement.html',
-						controller:"candidatePlacementCtrl"
+						controller:"candidatePlacementCtrl",
+							resolve: {
+            loginRequired: loginRequired
+            }
 					}
 				}
 			})
@@ -40,13 +55,15 @@
 			.state("index.jobPlacement",{
 				url:"/jobPlacement/:profession",
 				params:{
-					profession:null,
-					candidateid:null
+					profession:null
 				},
 				views: {
 					'content@': {
 						templateUrl:'./placementProcess/template/jobPlacement.html',
-						controller:"jobCtrl"
+						controller:"jobCtrl",
+							resolve: {
+              loginRequired: loginRequired
+            }
 					}
 				}
 			})
@@ -58,31 +75,10 @@
 				views: {
 					'content@': {
 						templateUrl:'./placementProcess/template/candidate.html',
-						controller:"candidatePlacementCtrl"
-					}
-				}
-			})
-			.state("index.appliedCandidate",{
-				url:"/appliedCandidate/",
-				params:{
-					jobcode:null
-				},
-				views: {
-					'content@': {
-						templateUrl:'./placementProcess/template/appliedCandidate.html',
-						controller:"appliedCandidateCtrl"
-					}
-				}
-			})
-			.state("index.appliedJob",{
-				url:"/appliedJob/",
-				params:{
-					candidateid:null
-				},
-				views: {
-					'content@': {
-						templateUrl:'./placementProcess/template/appliedJob.html',
-						controller:"appliedJobCtrl"
+						controller:"candidatePlacementCtrl",
+							resolve: {
+              loginRequired: loginRequired
+            }
 					}
 				}
 			})
