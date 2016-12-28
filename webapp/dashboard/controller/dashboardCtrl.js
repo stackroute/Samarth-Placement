9e3ba89296f11e0cb294b6c36698fe653df93ed6
kumaridevi <mkumaridevi11@gmail.com>
2016-12-28 12:25:23 +0000

    removed merge conflict

--- webapp/dashboard/controller/dashboardCtrl.js
+++ webapp/dashboard/controller/dashboardCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('samarth.dashboard')
 .controller('dashboardCtrl',dashboardCtrl);
 
@@ -39,6 +38,7 @@ function dashboardCtrl($scope, $mdDialog, $log, circlesGetService,$rootScope,$au
 }, function(err) {
 
 });
+}
 
 // angular.module('samarth.dashboard')
 //   .controller('dashboardCtrl',dashboardCtrl);
@@ -88,73 +88,3 @@ function dashboardCtrl($scope, $mdDialog, $log, circlesGetService,$rootScope,$au
 //
 //
 //
-       angular.module('samarth.dashboard')
-.controller('dashboardCtrl', dashboardCtrl);
-
-function dashboardCtrl($scope, $mdDialog, $log, circlesGetService, $rootScope, $auth, $location, $state) {
-$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
-      // console.log("kumari");
-      //if user is already traversing to index stage, ignore this check
-      //Here ignore all those states, which need not have authentication
-      if (toState.name == 'index') {
-          //index state does not need prior authentication
-          $location.path('/home/dashboard');
-          console.log("sdggdfgfdhgf");
-        }
-      });
-
-$rootScope.user = $auth.getPayload();
-console.log($rootScope.user);
-
-if ($auth.isAuthenticated()) {
-  $rootScope.sideicon = true;
-  $rootScope.logout = true;
-  console.log("hyhy");
-
-} else {
-  $rootScope.logout = false;
-  $rootScope.sideicon = false;
-  console.log("noo");
-  $rootScope.err = "invalid username or password";
-  $state.go('index');
-}
-  //  circlesGetService.getCircle()
-  //     .then(function(response) {
-  //         $scope.profiling = response.data;
-  //
-  //         console.log(response.data);
-  //
-  //     }, function(err) {
-  //
-  //     });
-  circlesGetService.getCircle()
-  .then(function(response) {
-    $scope.profiling = response.data;
-    var profs="";
-    for (var i = 0; i < response.data.length; i++) {
-      profs += response.data[i].name + "-";
-    }
-
-    circlesGetService.getCountData(profs)
-    .then(function(response) {}, function(err) {
-
-    });
-
-    circlesGetService.getCandidate(profs)
-    .then(function(response) {}, function(err) {
-
-    });
-       circlesGetService.getAppliedData(profs)
-    .then(function(response) {}, function(err) {
-
-    });
-    //      circlesGetService.getStatus(profs)
-    // .then(function(response) {}, function(err) {
-
-    // });
-
-  }, function(err) {
-
-  });
-
-};
--- webapp/home/controller/navbarCtrl.js
+++ webapp/home/controller/navbarCtrl.js
@@ -1,13 +1,11 @@
 (function(){
   'use strict'
   angular
-<<<<<<< HEAD
   .module('samarth.home') 
   .controller('navbarCtrl', navbarCtrl);
   function navbarCtrl($http,navFactory,$mdSidenav,$rootScope,$state,$auth,$scope) 
   {
 
-    
    console.log("nav");
    $rootScope.logout=false;
    if($auth.isAuthenticated())
@@ -60,101 +58,3 @@
 }
 
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-    .module('samarth.home')
-    .controller('navbarCtrl', navbarCtrl);
-    function navbarCtrl($http,navFactory,$mdSidenav,$rootScope,$state,$auth,$scope)
-    {
-
-
-      if($auth.isAuthenticated())
-    {
-      /*console.log("nav");
-            $rootScope.$on('$stateChangeStart','$location', function(event, toState, toParams, fromState,$location) {
-                console.log("kumari");
-                //if user is already traversing to index stage, ignore this check
-                //Here ignore all those states, which need not have authentication
-                if (toState.name == 'index') {
-                    //index state does not need prior authentication
-                    $location.path('/home/dashboard');
-                    console.log("sdggdfgfdhgf");
-                }
-
-                });*/
-
-      $state.go('index.dashboard');
-
-}
-               $scope.message="";
-               $rootScope.user=$auth.getPayload();
-               console.log($rootScope.user);
-               $scope.message=$rootScope.user.name;
-               console.log($scope.message);
-       var vm=this;
-       var navItems={};
-       $rootScope.sideicon = false;
-       $rootScope.logout = false;
-       vm.openSideNavPanel=openSideNavPanel;
-
-       vm.closeSideNavPanel=closeSideNavPanel;
-       vm.getSidenav=getSidenav;
-       vm.logout=logout;
-       vm.getSidenav();
-        $scope.openMenu = function($mdOpenMenu, ev) {
-                $mdOpenMenu(ev);
-            }
-
-       function getSidenav()
-       {
-        navFactory.getSidenav().then(function(response) {
-        vm.navItems=response.data;
-        });
-        }
-        function openSideNavPanel() {
-        $mdSidenav('left').open();
-        };
-
-        function closeSideNavPanel() {
-        $mdSidenav('left').close();
-        };
-
-      function logout()
-     {
-       $rootScope.sideicon = false;
-       $rootScope.logout = false;
-       $state.go('index');
-       $auth.removeToken();
-     }
-    }
-
-})();
->>>>>>> 4f744d65bd54891111b9da0697d9be3f6ae50846
