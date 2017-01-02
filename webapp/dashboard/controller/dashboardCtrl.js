869569cb5ed83f8100b5afae836710ba0af0415a
kumaridevi <mkumaridevi11@gmail.com>
2017-01-02 06:25:04 +0000

    modified dashboard

--- webapp/dashboard/controller/dashboardCtrl.js
+++ webapp/dashboard/controller/dashboardCtrl.js
@@ -1,75 +1,42 @@
 angular
-  .module('samarth.dashboard')
-  .controller('dashboardCtrl', dashboardCtrl);
+ .module('samarth.dashboard')
+ .controller('dashboardCtrl', dashboardCtrl);
 
-  function dashboardCtrl($scope, $mdDialog, $log, circlesGetService, $rootScope, $auth, $location, $state) {
-
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
-      if (toState.name == 'index') {
-        $location.path('/home/dashboard');
-      }
-    });
-    
-    $rootScope.user=$auth.getPayload();
-     $rootScope.message=$rootScope.user.name;
-     console.log($rootScope.message+"namedas");
-      if ($auth.isAuthenticated()) {
-        $rootScope.sideicon = true;
-        $rootScope.logout = true;
-      } 
-      else {
-        $rootScope.logout = false;
-        $rootScope.sideicon = false;
-        $rootScope.err = "invalid username or password";
-        $state.go('index');
-      }
-      
-      let arr=[];
-      circlesGetService.getCircle()
-      .then(function(response) {
-        let profs="";
-        for (var i = 0; i < response.data.length; i++) {
-          profs += response.data[i].name + "-";
-
-    // try {
-      $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
-        if (toState.name == 'index') {
-          $location.path('/home/dashboard');
-        }
-      });
-      $rootScope.user = $auth.getPayload();
-        if ($auth.isAuthenticated()) {
-          $rootScope.sideicon = true;
-          $rootScope.logout = true;
-        } 
-        else {
-          $rootScope.logout = false;
-          $rootScope.sideicon = false;
-          $rootScope.err = "invalid username or password";
-          $state.go('index');
-        }
-        
-        let arr=[];
-        circlesGetService.getCircle()
-        .then(function(response) {
-          let profs="";
-          for (var i = 0; i < response.data.length; i++) {
-            profs += response.data[i].name + "-";
-          }
-          circlesGetService.getStats(profs)
-          .then(function(response) {
-            console.log('final Data', response.data);
-            $scope.profiling = response.data;
-          },
-           function(err) {
-            console.log(err);
-          });
-        }, 
+ function dashboardCtrl($scope, $mdDialog, $log, circlesGetService, $rootScope, $auth, $location, $state) {
+   $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
+     if (toState.name == 'index') {
+       $location.path('/home/dashboard');
+     }
+   });
+   $rootScope.user = $auth.getPayload();
+     if ($auth.isAuthenticated()) {
+       $rootScope.sideicon = true;
+       $rootScope.logout = true;
+     } 
+     else {
+       $rootScope.logout = false;
+       $rootScope.sideicon = false;
+       $rootScope.err = "invalid username or password";
+       $state.go('index');
+     }
+     
+     let arr=[];
+     circlesGetService.getCircle()
+     .then(function(response) {
+       let profs="";
+       for (var i = 0; i < response.data.length; i++) {
+         profs += response.data[i].name + "-";
+       }
+       circlesGetService.getStats(profs)
+       .then(function(response) {
+         console.log('final Data', response.data);
+         $scope.profiling = response.data;
+       },
         function(err) {
-          console.log(err);
-        });
-    // }
-    // catch(e){
-    //   console.log()
-    // }
-  };
+         console.log(err);
+       });
+     }, 
+     function(err) {
+       console.log(err);
+     });
+ };
