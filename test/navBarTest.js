20e0e1a9039dc14bc8238835a7c3e5541bb8493c
kumaridevi <mkumaridevi11@gmail.com>
2017-01-02 06:38:41 +0000

    resolved navbar api

--- test/navBarTest.js
+++ test/navBarTest.js
@@ -4,7 +4,49 @@ var request = require('supertest');
 var host_url = 'http://localhost:8080'
 
 container=request(host_url);
-var msg={"role":"coordinator","sidenavmenuitems":[{"icon":"dashboard","navTitle":"Dashboard","traverse":"index.dashboard"},{"icon":"search","navTitle":"Candidate Search","traverse":"index.candidatesearch"},{"icon":"person_add","navTitle":"Candidate Register","traverse":"index.candidateReg"},{"icon":"add box","navTitle":"Job Post","traverse":"index.jobPost"},{"icon":"youtube_searched_for_black","navTitle":"Job Search","traverse":"index.jobSearch"},{"icon":"person_pin","navTitle":"JobProvider Registration","traverse":"index.empreg"}]};
+var msg={
+  "role": "coordinator",
+   "sidenavmenuitems":[
+{
+  "icon":"dashboard",
+  "navTitle":"Dashboard",
+  "traverse":"index.dashboard"
+},
+{
+  "icon":"search",
+  "navTitle":"Candidate search",
+  "traverse":"index.candidatesearch"
+},
+{
+  "icon":"person_add",
+  "navTitle":"Candidate register",
+  "traverse":"index.candidateReg"
+},
+{
+  "icon":"add box",
+  "navTitle":"Job post",
+  "traverse":"index.jobPost"
+},
+{
+  "icon":"youtube_searched_for_black",
+  "navTitle":"Job search",
+  "traverse":"index.jobSearch"
+},
+
+{
+  "icon":"person_pin",
+  "navTitle":"Jobprovider",
+  "traverse":"index.emp"
+},
+
+{
+  "icon":"local_library",
+  "navTitle":"Aboutus",
+  "traverse":"index.aboutus"
+}
+]
+      };
+
 var container_url = host_url + '/sidenavbar';
 	describe(' checking navbar', function(){
 		
