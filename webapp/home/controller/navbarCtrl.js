ba25cdb48adab8c5cb4dbb07caf868859cb4d04d
kumaridevi <mkumaridevi11@gmail.com>
2017-01-03 06:55:34 +0000

    removed lint errors

--- webapp/home/controller/navbarCtrl.js
+++ webapp/home/controller/navbarCtrl.js
@@ -1,58 +1,57 @@
-(function(){
+(function() {
   'use strict'
   angular
   .module('samarth.home') 
   .controller('navbarCtrl', navbarCtrl);
-  function navbarCtrl($http,navFactory,$mdSidenav,$rootScope,$state,$auth,$scope) 
+  function navbarCtrl(navFactory,
+    $auth,
+    $mdSidenav,
+    $rootScope,
+    $state) 
   {
-
-   $rootScope.logout=false;
+   $rootScope.logout = false;
    if($auth.isAuthenticated())
    {
-    
-     
-     $rootScope.user=$auth.getPayload();
-     $rootScope.message=$rootScope.user.name;
-     console.log($rootScope.message+"name");
+     $rootScope.user = $auth.getPayload();
+     $rootScope.message = $rootScope.user.name;
      $state.go('index.dashboard');
-     
    }
-   else{
+   else
+   {
     $state.go('index.home');
   }
-  
-  var vm=this;
-  var navItems={};
-  
-  vm.openSideNavPanel=openSideNavPanel;
-  
-  vm.closeSideNavPanel=closeSideNavPanel;
-  vm.getSidenav=getSidenav;
-  vm.logout=logout;
+
+  let vm = this;
+  vm.openSideNavPanel = openSideNavPanel;
+  vm.closeSideNavPanel = closeSideNavPanel;
+  vm.getSidenav = getSidenav;
+  vm.logout = logout;
   vm.getSidenav();
-  
 
   function getSidenav()
   {
-    navFactory.getSidenav().then(function(response) {
-      vm.navItems=response.data;
+    navFactory.then(function(response)
+     {
+      vm.navItems = response.data;
     });
-  }
-  function openSideNavPanel() {
+  };
+
+  function openSideNavPanel()
+   {
     $mdSidenav('left').open();
   };
-  
-  function closeSideNavPanel() {
+
+  function closeSideNavPanel() 
+  {
     $mdSidenav('left').close();
   };
-  
+
   function logout()
   {
    $rootScope.sideicon = false;
    $rootScope.logout = false;
    $state.go('index.home');
    $auth.removeToken();
- }
+ };
 }
-
 })();
